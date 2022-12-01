26d5b7214deaf9308705ae7cf5a63d2d86e44963
Tai Klein <etai@hioscar.com>
2022-12-01 17:45:45 -0500

    day1

--- solutions/day01.dart
+++ solutions/day01.dart
@@ -5,19 +5,50 @@ class Day01 extends GenericDay {
 
   @override
   parseInput() {
-
+    return new InputUtil(1).getPerLine();
   }
 
   @override
   int solvePart1() {
-
-    return 0;
+    var lines = parseInput();
+    int currentBest = 0;
+    int currentBestIndex = -1;
+    int current = 0;
+    for (int i = 0; i < lines.length; i++) {
+      if (lines[i] == '') {
+        if (currentBest < current) {
+          currentBest = current;
+          currentBestIndex = i;
+        }
+        current = 0;
+      } else {
+        current += int.parse(lines[i]);
+      }
+    }
+    return currentBest;
   }
 
   @override
   int solvePart2() {
-
-    return 0;
+    var lines = parseInput();
+    List<int> allBest = [];
+    int current = 0;
+    for (int i = 0; i < lines.length; i++) {
+      if (lines[i] == '') {
+        allBest.add(current);
+        current = 0;
+      } else {
+        current += int.parse(lines[i]);
+      }
+    }
+    allBest.sort();
+    var rankedBest = new List.from(allBest.reversed);
+    var top3sum = rankedBest[0] + rankedBest[1] + rankedBest[2];
+    return top3sum;
   }
 }
 
+void main() {
+  print(Day01().solvePart1());
+  print(Day01().solvePart2());
+}
